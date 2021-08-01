6f1e9614c7b315688bdb35b7d185e5dad8284fc2
kiishi <deolu.asenuga@gmail.com>
2021-08-01 08:15:55 +0100

    clean up

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@
 export default {
     port: process.env.PORT || 3000,
     database:{
-        url: process.env.DATABASE_URI || "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true"
+        url: process.env.DATABASE_URI
     }
 }
